2fdaacba77e329c719f6fded53894a107b000e1d
sourav <sourav10010080@gmail.com>
2025-06-07 22:32:48 +0530

    added routing

---
+++ Week3/routing/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import PostDetails from "./pages/PostDetails";
+
+const App = () => (
+  <Router>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/post/:id" element={<PostDetails />} />
+    </Routes>
+  </Router>
+);
+
+export default App;
---
+++ Week3/routing/pages/About.jsx
@@ -0,0 +1,9 @@
+const About = () => (
+  <div style={{ padding: "1rem" }}>
+    <h2>About This Project</h2>
+    <p>This is a simple React blog app built for learning routing and state filtering.</p>
+    <p>Author: Your Name</p>
+  </div>
+);
+
+export default About;
